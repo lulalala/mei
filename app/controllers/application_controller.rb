1a53a2ee16b24482a8a904685998ba70b3ed339c
lulalala <mark@goodlife.tw>
2015-03-29 17:23:30 +0800

    Globally load board. Use seo_name as url param. Set that seo_name as default url settings.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_board
+  def set_board
+    @board = Board.find_by_seo_name!(params[:board])
+    default_url_options[:board] = params[:board]
+  end
 end
--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,7 @@
 class Board < ActiveRecord::Base
   has_many :topics, inverse_of: :board
+
+  def to_param
+    seo_name
+  end
 end
