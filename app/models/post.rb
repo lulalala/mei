0db748d1c40e2eb3ae1a6ea7dd54f3b9c9385d42
lulalala <mark@goodlife.tw>
2015-04-24 22:20:20 +0800

    Ignore photo insertion if no images are present in POST params

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,9 @@ class Post < ActiveRecord::Base
   belongs_to :topic, inverse_of: :posts
   has_many :images, inverse_of: :post
 
-  accepts_nested_attributes_for :images
+  accepts_nested_attributes_for :images, reject_if: proc{|attributes|
+    attributes.values_at(:remote_image_url, :image).all? &:blank?
+  }
 
   after_save :bump_topic
   def bump_topic
