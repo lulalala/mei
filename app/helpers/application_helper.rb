bc7edc120befba589ba4f16a4c0c73735139569a
lulalala <mark@goodlife.tw>
2015-04-24 20:43:46 +0800

    Label body class with controller/action names.
    For page specific js/css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,9 @@ module ApplicationHelper
   def render_view_fragment(name)
     render_cell(:view_fragment, :show, @board, name)
   end
+
+  def render_body_classes
+    classes = [ params[:controller].tr('/','_'), params[:action] ]
+    classes.join(' ')
+  end
 end
