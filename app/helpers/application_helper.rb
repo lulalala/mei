ef8b4bdc75d44f84db71814568afbcb5c158f2d0
lulalala <mark@goodlife.tw>
2015-05-04 21:00:12 +0800

    Render custom view fragments from partial first.
    Benefits are:
    Easier to edit.
    Different formats can be rendered such as erb.
    
    Renamed location for consistency.
    Add bottom_of_head for custom script insertion

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,12 +3,24 @@ module ApplicationHelper
     simple_format source
   end
 
-  def render_view_fragment(name)
-    render_cell(:view_fragment, :show, @board, name)
-  end
-
   def render_body_classes
     classes = [ params[:controller].tr('/','_'), params[:action] ]
     classes.join(' ')
+  end
+
+  # Renders custom HTML content.
+  #
+  # Order of precedence:
+  # in app/views/partials/_global/
+  # in app/views/partials/{board seo name}/
+  # in view_fragments table in database
+  def render_custom_partial(name)
+    ["partials/#{@board.seo_name}/#{name}", "partials/_global/#{name}"].each do |path|
+      if lookup_context.template_exists?(path, nil, true)
+        return render(partial:path)
+      end
+    end
+
+    render_cell(:view_fragment, :show, @board, name)
   end
 end
