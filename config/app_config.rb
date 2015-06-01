387c77020673c4c04a166dca28a8679229819976
lulalala <mark@goodlife.tw>
2015-06-01 23:06:10 +0800

    Add domain config.
    There are times when app need to access domain settings
    outside of a request (e.g. background worker)
    This is a way to set domain information.

--- config/app_config.rb
+++ config/app_config.rb
@@ -1,4 +1,18 @@
 require 'settingslogic'
 class AppConfig < Settingslogic
   source File.dirname(__FILE__) << "/app_config.yml"
+
+  # returns domain string
+  # pass in false for segment key to hide that segment, e.g. `protocol:false`
+  def self.domain(params = {})
+    default_params = self.get('domain_setting').to_hash
+    params = default_params.merge(params)
+
+    url = ''
+    url << params[:protocol].clone << '://' if params[:protocol]
+    url << params[:subdomain].clone << '.' if params[:subdomain]
+    url << params[:domain]
+    url << ':' << params[:port].to_s if params[:port]
+    url
+  end
 end
