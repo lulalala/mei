8e7b11747d1db1def51244eea6ed8da262d4f70d
lulalala <mark@goodlife.tw>
2015-09-20 14:01:29 +0800

    Spec for board config being independent to each other

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -11,5 +11,19 @@ RSpec.describe Board, type: :model do
       subject.config = {'foo' => 2}
       subject.config[:foo].should == 2
     end
+    it "does not interfere with other board configs" do
+      AppConfig['board']['foo'] = 1
+
+      board1 = FactoryGirl.create(:board, config: {'foo' => 7})
+      board2 = FactoryGirl.create(:board)
+
+      board2.config[:foo].should == 1
+
+      board3 = FactoryGirl.create(:board, config: {'foo' => 9})
+
+      board3.config[:foo].should == 9
+      board2.config[:foo].should == 1
+      board1.config[:foo].should == 7
+    end
   end
 end
