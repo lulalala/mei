27758c1020d2d5c6a279f060cbd55ea18c4ebaeb
lulalala <mark@goodlife.tw>
2016-03-27 19:58:47 +0800

    Set thumbnail width and height in view
    To prevent layout reflow

--- app/presenters/image_presenter.rb
+++ app/presenters/image_presenter.rb
@@ -1,7 +1,7 @@
 class ImagePresenter < LulalalaPresenter::Base
   def linked_display
     h.link_to(
-      h.image_tag(h.image_url(model.image.thumb.url)),
+      h.image_tag(h.image_url(model.image.thumb.url), width:model.thumb_width, height:model.thumb_height),
       h.image_url(model.image.url),
       class:'img'
     ).html_safe
